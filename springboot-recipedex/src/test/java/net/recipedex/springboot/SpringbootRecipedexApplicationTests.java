1136f31e53ec2bb2979061dd81df99565462a90b
morbe65 <61469436+morbe65@users.noreply.github.com>
2022-06-18 10:27:45 -0700

    environment setup

---
+++ springboot-recipedex/src/test/java/net/recipedex/springboot/SpringbootRecipedexApplicationTests.java
@@ -0,0 +1,13 @@
+package net.recipedex.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRecipedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
